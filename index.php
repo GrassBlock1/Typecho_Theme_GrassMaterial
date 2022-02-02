3787eee50ed352865c3ec4d005bf8f02b72a151c
郭桓桓 <hi@nekohuan.cyou>
2022-02-02 22:14:35 +0800

    Solve: MDUI cdn breaked.

--- index.php
+++ index.php
@@ -8,8 +8,20 @@
  * @link https://blog.acfk.xyz/
  */
 $this->need('header.php');?>
-	<!DOCTYPE HTML>
-		<!--标准开头-->
+<!-- MDUI CSS -->
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/css/mdui.min.css"
+  integrity="sha384-cLRrMq39HOZdvE0j6yBojO4+1PrHfB7a9l5qLcmRm/fiWXYY+CndJPmyu5FV/9Tw"
+  crossorigin="anonymous"
+/>
+
+<!-- MDUI JavaScript -->
+<script
+  src="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js"
+  integrity="sha384-gCMZcshYKOGRX9r6wbDrvF+TcCCswSHFucUzUPwka+Gr+uHgjlYvkABr95TCOz3A"
+  crossorigin="anonymous"
+></script>		<!--标准开头-->
 			<br>
 				<br>
 					
